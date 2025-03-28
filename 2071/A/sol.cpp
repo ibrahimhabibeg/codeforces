31fc1fd682dfc7a497f36f5faf48e359a1f1d316
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2025-03-28 15:22:07 +0200

    2071 A

---
+++ 2071/A/sol.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+  int n;
+  scanf("%d", &n);
+  if(n%3==1) printf("YES\n");
+  else printf("NO\n");
+}
+
+int main(){
+  int TC;
+  scanf("%d", &TC);
+  while(TC--){
+    solve();
+  }
+}
