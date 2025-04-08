78a0cd61cff6bb15b31c8a76aaa6256793217024
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2025-04-08 21:30:54 +0200

    2086 A

---
+++ 2086/A/sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+  int n;
+  scanf("%d", &n);
+  printf("%d\n", 2*n);
+}
+
+int main(){
+  int TC;
+  scanf("%d", &TC);
+  while(TC--){
+    solve();
+  }
+}
